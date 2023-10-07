dd9111a573078ea71ccbaf450d1e2bbcfecba13a
Aishwarya Rao <aishwarya.rao@sjsu.edu>
2023-10-07 20:40:07 +0800

    Send response body

--- app/server.go
+++ app/server.go
@@ -5,14 +5,10 @@ import (
 	"net"
 	"os"
 	"strings"
+	"log"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -21,33 +17,42 @@ func main() {
 	
 	conn, err := l.Accept()
 	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Accepted new Connection")
+
+	buf := make([]byte, 4096)
+	_, err = conn.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := strings.Split(string(buf), "\r\n")
+	path := strings.Fields(lines[0])[1]
+
+
+	switch {
+	case path == "/":
+		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	case strings.HasPrefix(path, "/echo"):
+		{
+			text := strings.TrimPrefix(path, "/echo/")
+			size := fmt.Sprint(len(text))
+			_, err = conn.Write([]byte("HTTP/1.1 200 OK \r\nContent-Type: text/plain\r\nContent-Length: " + size + "\r\n\r\n" + text + "\r\n"))
+		}
+	default:
+		_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-
-	buffer := make([]byte, 1024)
-	_, err = conn.Read(buffer)
-	if err != nil {
-		fmt.Println("Error reading: ", err.Error())
-		os.Exit(1)
-	}
-
-	req := strings.Split(string(buffer), " ")
-
-	if req[1] == "/" {
-		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		if err != nil {
-			fmt.Println("Error writing:", err.Error())
-			os.Exit(1)
-		}
-	} else {
-		_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		if err != nil {
-			fmt.Println("Error writing:", err.Error())
-			os.Exit(1)
-		}
-	}
-
 	
 	
 }
