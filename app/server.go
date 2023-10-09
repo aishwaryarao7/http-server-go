8f5e9c4bd2c7e6d2fe2f4bec4f9ac69b11ea7e93
Aishwarya Rao <aishwarya.rao@sjsu.edu>
2023-10-10 01:08:45 +0800

    Parse header and return in the response

--- app/server.go
+++ app/server.go
@@ -2,11 +2,40 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
+	"net/http"
 	"os"
 	"strings"
-	"log"
 )
+
+func getURLHeaders(url string) map[string]interface{} {
+	response, err := http.Head(url)
+	if err != nil {
+		log.Fatal("Error: Unable to download URL (", url, ") with error: ", err)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		log.Fatal("Error: HTTP Status = ", response.Status)
+	}
+
+	headers := make(map[string]interface{})
+
+	for k, v := range response.Header {
+		headers[strings.ToLower(k)] = string(v[0])
+	}
+
+	return headers
+}
+
+func getUserAgent() {
+	headers := getURLHeaders("/user-agent")
+	fmt.Println(headers)
+	_, err := conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n" + headers["user-agent"].(string) + "\r\n"))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -14,7 +43,7 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
+
 	conn, err := l.Accept()
 	if err != nil {
 		log.Fatal(err)
@@ -28,13 +57,21 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	if strings.HasPrefix(string(buf), "/user-agent") {
+		// Respond with a 200 OK response and content type of text/plain
+		getUserAgent()
+		return
+	}
 	lines := strings.Split(string(buf), "\r\n")
 	path := strings.Fields(lines[0])[1]
-
 
 	switch {
 	case path == "/":
 		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	case strings.HasPrefix(path, "/user-agent"):
+		{
+			getUserAgent()
+		}
 	case strings.HasPrefix(path, "/echo"):
 		{
 			text := strings.TrimPrefix(path, "/echo/")
@@ -53,6 +90,5 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	
-	
+
 }
